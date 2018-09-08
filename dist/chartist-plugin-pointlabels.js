442297b44d11e805faad1d970db703d34bb88c3b
Jimmy Truong <thamestruong@gmail.com>
2018-09-08 00:46:21 -0700

    Handle y=0 while x=undefined with care

--- dist/chartist-plugin-pointlabels.js
+++ dist/chartist-plugin-pointlabels.js
@@ -71,7 +71,9 @@
         // if x and y exist concat them otherwise output only the existing value
         var value = data.value.x !== undefined && data.value.y ?
           (data.value.x + ', ' + data.value.y) :
-          data.value.y || data.value.x;
+          (data.value.y !== undefined ?
+            data.value.y :
+            data.value.x)
 
         data.group.elem('text', {
           x: position.x + options.labelOffset.x,
